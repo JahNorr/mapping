0c2c7b2da4673813252177dccfcd257fc70d628c
jahNorr <John.Orr@doh.vi.gov>
2015-10-15 13:40:07 -0400

    google work

--- mappingGoogle01.R
+++ mappingGoogle01.R
@@ -6,16 +6,29 @@
 
 
 library(maptools)
+library(RColorBrewer)
+library(ggmap)
+
+url<-"http://www2.census.gov/geo/tiger/TIGER2014/ESTATE/tl_2014_78_estate.zip"
+destfile<-"./data_raw/zip/tl_2014_78_estate.zip"
+method<-"auto"
+
+download.file(url, destfile, method, quiet = FALSE, mode = "w",
+              cacheOK = TRUE)
+
+exdir<-"./data_raw"
+
+unzip(destfile,exdir=exdir)
+
+
 area <- readShapePoly("C:/Users/john/Documents/Geodata/VI/tl_2014_78_estate/tl_2014_78_estate.shp")
 
 # # or file.choose:
 # area <- readShapePoly(file.choose())
 
 
-library(RColorBrewer)
 colors <- brewer.pal(9, "BuGn")
 
-library(ggmap)
 mapImage <- get_map(location = c(lon = -64.77, lat = 17.75),
                     color = "color",
                     source = "google",
